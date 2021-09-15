f5d96ba3e91320d6a035d857f9118dedc9484f40
joshnewnes <90755891+joshnewnes@users.noreply.github.com>
2021-09-15 18:12:05 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+from PIL import Image, ImageFilter
+import math
+
+image = Image.open('abc.png')
+image = image.filter(ImageFilter.FIND_EDGES)
+image = image.convert("1");
+image.save('output.png')
+width, height = image.size
+
+print(height)
+height2 = int(height)
+print(height2)
+width2 = int(width)
+pix = image.load()
+ind_pixels = []
+num_pixels = height2 * width2
+for j in range(height2):
+  for i in range(width2):
+    ind_pixels.append(pix[i,j])
+for q in range(num_pixels):
+  q_right = q+1
+  q_below = q+width2
+  column_right = (q_right % width2)
+  current_row = math.floor(q/width2)
+  if (current_row != height2):
+    below_row = current_row+1
+    below_valid = 1
+  else:
+    below_valid = 0
+  current_col = (q % width2)
+  current_pixel_value = ind_pixels[int(q)]
+  right_pixel_value = ind_pixels[int(q_right)]
+  below_pixel_value = ind_pixels[int(q_below)]
+  if (current_pixel_value == 255):
+    if (right_pixel_value == 255):
+      print("y=",current_row, r"\{" , current_col, "<= x <=", column_right, r"\}")
+    if (below_valid == 1):
+      if (below_pixel_value == 255):
+        print("x=",current_col, r"\{" , current_row, "<= y <=", below_row, r"\}")
+      
+ 
+    
+    
+
